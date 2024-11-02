81ded8282a9b25c33ee7d8e235487a912aaebe0c
Kogepan229 <kogepan25629@gmail.com>
2024-11-02 21:28:19 +0900

    Make validation strict

--- main/src/features/admin/schema.ts
+++ main/src/features/admin/schema.ts
@@ -2,7 +2,11 @@ import { roomsTable } from "@/db/schema";
 import { createInsertSchema } from "drizzle-zod";
 import type { z } from "zod";
 
-export const createRoomSchema = createInsertSchema(roomsTable).pick({
+export const createRoomSchema = createInsertSchema(roomsTable, {
+  name: (schema) => schema.name.trim().min(1, "1文字以上必要です"),
+  description: (schema) => schema.description.trim().transform((s) => (s === "" ? null : s)),
+  accessKey: (schema) => schema.accessKey.trim().min(1, "1文字以上必要です"),
+}).pick({
   name: true,
   description: true,
   roomType: true,
