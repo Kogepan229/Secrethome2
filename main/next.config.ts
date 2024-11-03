4f75702d3bbe72f47740191643f361faaf04724d
Kogepan229 <kogepan25629@gmail.com>
2024-11-03 23:38:11 +0900

    Import SVG as Component

--- main/next.config.ts
+++ main/next.config.ts
@@ -1,10 +1,26 @@
 import type { NextConfig } from "next";
-import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
+import { createVanillaExtractPlugin } from "@vanilla-extract/next-plugin";
 
 const withVanillaExtract = createVanillaExtractPlugin();
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+  experimental: {
+    turbo: {
+      rules: {
+        "*.svg": {
+          loaders: ["@svgr/webpack"],
+          as: "*.js",
+        },
+      },
+    },
+  },
 };
 
 export default withVanillaExtract(nextConfig);
