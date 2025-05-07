e13c6d9caae4f455f08d02fd5337e6768e63b4fb
Kogepan229 <kogepan25629@gmail.com>
2025-05-07 17:40:50 +0900

    Add color variant to BasicButton

--- main/src/components/BasicButton.tsx
+++ main/src/components/BasicButton.tsx
@@ -9,6 +9,7 @@ export const buttonStyle = tv({
         "bg-white border border-border-primary text-black hover:bg-hover-gray active:bg-active-gray disabled:text-disabled-gray disabled:border-border-dark-gray disabled:bg-hover-gray",
       whiteRed:
         "bg-white border border-red-400 text-black hover:bg-hover-gray active:bg-active-gray disabled:text-disabled-gray disabled:border-border-dark-gray disabled:bg-hover-gray",
+      primary: "bg-primary text-white-primary hover:bg-hover-primary",
     },
   },
   defaultVariants: {
