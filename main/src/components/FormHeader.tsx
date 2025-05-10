92d43c572a27bab28ad08f5de1a46aaf389f8856
Kogepan229 <kogepan25629@gmail.com>
2025-05-10 21:20:02 +0900

    Add Formheader

---
+++ main/src/components/FormHeader.tsx
@@ -0,0 +1,3 @@
+export const FormHeader = ({ title }: { title: string }) => {
+  return <h1 className="w-full h-10 mt-12 px-2 bg-primary text-white-primary text-lg leading-10 font-bold">{title}</h1>;
+};
