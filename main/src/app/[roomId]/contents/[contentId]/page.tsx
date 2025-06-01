f281c4dea477e0dfb9d74cbf99dc60a5c01d3d34
Kogepan229 <kogepan25629@gmail.com>
2025-06-01 23:18:27 +0900

    Style title and description

--- main/src/app/[roomId]/contents/[contentId]/page.tsx
+++ main/src/app/[roomId]/contents/[contentId]/page.tsx
@@ -2,6 +2,7 @@ import { db } from "@/db/db";
 import { contentsTable } from "@/db/schema";
 import { VideoPlayer } from "@/features/room/video/components/VideoPlayer";
 import { and, eq } from "drizzle-orm";
+import Linkify from "linkify-react";
 
 export default async function ContentPage({
   params,
@@ -15,6 +16,7 @@ export default async function ContentPage({
       .from(contentsTable)
       .where(and(eq(contentsTable.id, contentId), eq(contentsTable.status, "available")))
   ).at(0);
+
   if (!content) {
     return <div>コンテンツが存在しません</div>;
   }
@@ -23,10 +25,12 @@ export default async function ContentPage({
     <div className="w-[90%] max-w-220 m-auto">
       <VideoPlayer src={`${process.env.NEXT_PUBLIC_FILES_URL}/video/${contentId}/playlist.m3u8`} />
       <div>
-        <div>
-          <span>{content.title}</span>
+        <div className="mt-1 text-black-primary">
+          <span className="text-2xl font-bold">{content.title}</span>
         </div>
-        <span>{content.description}</span>
+        <div className="mt-2 p-2 rounded-md bg-gray-200/60 [&>a]:text-cyan-700">
+          <Linkify options={{ nl2br: true, target: "_blank" }}>{content.description}</Linkify>
+        </div>
       </div>
     </div>
   );
