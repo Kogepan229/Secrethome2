41c0eb0d206986273619c7b19cef0b07168ccd08
Kogepan229 <kogepan25629@gmail.com>
2025-10-25 23:22:11 +0900

    chore(front): Update deps

--- main/next.config.ts
+++ main/next.config.ts
@@ -1,28 +1,22 @@
 import { URL } from "node:url";
 import type { NextConfig } from "next";
+import { PHASE_DEVELOPMENT_SERVER } from "next/constants";
 
-const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [new URL("http://localhost:20080/**")],
-  },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      use: ["@svgr/webpack"],
-    });
-    return config;
-  },
-  turbopack: {
-    rules: {
-      "*.svg": {
-        loaders: ["@svgr/webpack"],
-        as: "*.js",
+module.exports = (phase: string) => {
+  const nextConfig: NextConfig = {
+    images: {
+      remotePatterns: [new URL("http://localhost:20080/**")],
+      dangerouslyAllowLocalIP: phase === PHASE_DEVELOPMENT_SERVER ? true : false,
+    },
+    turbopack: {
+      rules: {
+        "*.svg": {
+          loaders: ["@svgr/webpack"],
+          as: "*.js",
+        },
       },
     },
-  },
-};
+  };
 
-export default nextConfig;
+  return nextConfig;
+};
